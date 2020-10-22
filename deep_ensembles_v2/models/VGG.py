cbb84f6d0f3959950b5c745829aed93b0f0e81be
sbuschjaeger <sebastian.buschjaeger@tu-dortmund.de>
2020-10-22 10:58:15 +0200

    Added build/* to gitignore Added automatic computation of linear_size for VGG Added debug output in SKLearnModel if there is an exception during the forward pass

--- deep_ensembles_v2/models/VGG.py
+++ deep_ensembles_v2/models/VGG.py
@@ -7,19 +7,10 @@ from collections import OrderedDict
 from deep_ensembles_v2.Utils import Flatten
 
 class VGGNet(nn.Module):
-    # TODO Make this more generic with hidden_size / n_channels / depth
-    def __init__(self, in_channels = 3, model_size = "large"):
+    def __init__(self, input_size = (3,32,32), n_channels = 32, depth = 4, hidden_size = 512, p_dropout = 0.0, n_classes = 100):
         super().__init__()
 
-        if "large" in model_size:
-            hidden_size = 512
-            n_channels = 32
-            depth = 4
-        else:
-            hidden_size = 256
-            n_channels = 16
-            depth = 3
-
+        in_channels = input_size[0]
         def make_layers(level, n_channels):
             return [
                 nn.Conv2d(in_channels if level == 0 else level*n_channels, (level+1)*n_channels, kernel_size=3, padding=1, stride = 1, bias=True),
@@ -35,22 +26,40 @@ class VGGNet(nn.Module):
         for i in range(depth):
             model.extend(make_layers(i, n_channels))
 
-        if depth == 1:
-            lin_size = 128*n_channels
-        elif depth == 2:
-            lin_size = 64*n_channels
-        elif depth == 3:
-            lin_size = 48*n_channels
-        else:
-            lin_size = 16*n_channels
+        # This is kinda hacking but it works well to automatically determine the size of the linear layer
+        x = torch.rand((1,*input_size)).type(torch.FloatTensor)
+        for l in model:
+            x = l(x)
+        lin_size = x.view(1,-1).size()[1]
+        # in_channels = 1?
+        # if n_layers == 1:
+        #     lin_size = 506*n_channels #
+        # elif n_layers == 2:
+        #     lin_size = 242*n_channels
+        # elif n_layers == 3:
+        #     lin_size = 75*n_channels
+        # elif n_layers == 4:
+        #     lin_size = 16*n_channels
+        # else:
+        #     lin_size = 5*n_channels
+        # in_channels = 3?
+        # if depth == 1:
+        #     lin_size = 128*n_channels
+        # elif depth == 2:
+        #     lin_size = 64*n_channels
+        # elif depth == 3:
+        #     lin_size = 48*n_channels
+        # else:
+        #     lin_size = 16*n_channels
 
         model.extend(
             [
                 Flatten(),
                 nn.Linear(lin_size, hidden_size),
+                nn.Dropout(p_dropout) if p_dropout > 0 else None,
                 nn.BatchNorm1d(hidden_size),
                 nn.ReLU(),
-                nn.Linear(hidden_size, 100)
+                nn.Linear(hidden_size, n_classes)
             ]
         )
 
