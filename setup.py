dbbb0368646cc559e1eac20e75c2f5efc911169e
sbuschjaeger <sebastian.buschjaeger@tu-dortmund.de>
2020-11-06 16:19:29 +0100

    setup py fixed for new path

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ modules= []
 # if CUDA_HOME:
 modules.append(
       CUDAExtension('binarization', [
-            'deep_ensembles_v2/cuda/binarization/binarization.cpp',
-            'deep_ensembles_v2/cuda/binarization/binarize_cuda.cu',
+            'pysembles/cuda/binarization/binarization.cpp',
+            'pysembles/cuda/binarization/binarize_cuda.cu',
       ]),
 )
 
-setup(name='deep_ensembles_v2',
-      version='0.1',
+setup(name='Pysembles',
+      version='0.2',
       description='Common ensemble approaches for PyTorch models and some soft decision tree approaches. Also includes some code for training binarized neural networks. Many thanks at Mikail Yayla (mikail.yayla@tu-dortmund.de) for providing CUDA kernels for BNN training. He maintains a more evolved repository on BNNs - check it out at https://github.com/myay/BFITT',
       url='https://github.com/sbuschjaeger/deep_ensembles_v2/',
       author=u'Sebastian Buschjäger',
       author_email='sebastian.buschjaeger@tu-dortmund.de',
       license='MIT',
-      packages=['deep_ensembles_v2'],
+      packages=['pysembles'],
       zip_safe=False,
       install_requires = [
             'torch'
